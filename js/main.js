afd9dfe8c2da25ce575eae9084aac894db61cad7
Gavin Wright <gavingt@gmail.com>
2017-06-08 22:25:16 -0700

    About to add scroll spy

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(".nav-pills>*").click(function() {
+    $(".nav-pills>*").not(this).removeClass("active");  //remove "active" class from all nav-pills other than the one clicked
+
+    if (this.id === "portfolio_button") {
+        $('html, body').animate({
+            scrollTop: $(".portfolio").offset().top
+        }, 1000);
+    }
+});
+
