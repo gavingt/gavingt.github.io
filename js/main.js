52677eea59938edbbd91669713f8e00cc67d74fd
Gavin Wright <gavingt@gmail.com>
2018-09-13 07:21:35 -0700

    Updated lots of project stuff, rearranged them too.

--- js/main.js
+++ js/main.js
@@ -26,9 +26,39 @@ function initializeProjectButtons() {
     $(".all-projects").click(function() {
         if (this.classList.contains("project1")) {
             $(".carousel-inner").html(
-                "<div class='item active'><img src='img/project1/1.jpg' class='d-block img-fluid'> </div> <div class='item'> <img src='img/project1/2.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project1/3.jpg' class='d-block img-fluid'></div><div class='item'> <img src='img/project1/4.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project1/5.jpg' class='d-block img-fluid'></div>"
+                "<div class='item active'><img src='img/project1/1.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project1/2.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project1/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project1/4.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project1/5.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project1/6.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project1/7.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project1/8.jpg' class='d-block img-fluid'></div>"
+            );
+
+            $(".modal-footer").html("<div class='modal-title'>Delivery Tip Tracker<br>Pro version: <a href='http://goo.gl/KD2y1t' target='_blank'>goo.gl/KD2y1t</a> &nbsp;| &nbsp;Free version: <a href='https://goo.gl/E8xff3' target='_blank'>goo.gl/E8xff3</a></div>" +
+                "Delivery Tip Tracker is an Android app that helps delivery drivers quickly track their tips. Drivers enter their tips using voice or through a simple touch interface. The app uses GPS to calculate the distance driven on " +
+                "each delivery, yielding useful statistics such as average tip per mile driven and average distance per delivery. The Pro version stores comprehensive tip history with daily, monthly, yearly, and all-time views, and even " +
+                "the ability to see their &quot;high scores&quot; in various stats. Users also have access to cloud-based backup and restore functionality, so they never have to worry about losing their data.<br><br>" +
+                "As of September 2018, the Pro version has accumulated about 1,000 purchases at $2.99 and the Free version has about 6,000 installs. The Pro version has a user rating of 4.6/5 stars, while the Free version is rated 4.2/5 stars."
+            );
+        }
+        else if (this.classList.contains("project2")) {
+            $(".carousel-inner").html(
+                "<div class='item active'><img src='img/project2/1.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project2/2.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project2/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project2/4.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project2/5.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project2/6.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project2/7.jpg' class='d-block img-fluid'></div>"
+            );
+
+            $(".modal-footer").html("<div class='modal-title'>Store 7667 Delivery Maps<br><a href='http://goo.gl/nfQDfa' target='_blank'>goo.gl/nfQDfa</a></div>" +
+                "I wrote this Android app to help delivery drivers at the Domino's store I worked at. It contains all the apartment and hotel maps for our delivery area, allowing drivers " +
+                "to quickly find their destinations. The user can employ voice search or a touch menu to find the relevant map. The app also contains a list of known gate codes in the area, " +
+                "which any user can update on-the-fly. Store-wide use of my app led to a significant increase in on-time deliveries, an important metric for any delivery restaurant."
+
+            );
+        }
+        else if (this.classList.contains("project3")) {
+            $(".carousel-inner").html(
+                "<div class='item active'><img src='img/project3/1.jpg' class='d-block img-fluid'> </div> <div class='item'> <img src='img/project3/2.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project3/3.jpg' class='d-block img-fluid'></div><div class='item'> <img src='img/project3/4.jpg' class='d-block img-fluid'>" +
+                "</div><div class='item'><img src='img/project3/5.jpg' class='d-block img-fluid'></div>"
             );
 
             $(".modal-footer").html("<div class='modal-title'>ClassTask<br><a href='https://gavingt.github.io/ClassTask' target='_blank'>gavingt.github.io/ClassTask</a></div>ClassTask is a " +
@@ -36,48 +66,18 @@ function initializeProjectButtons() {
                 "giving the user an at-a-glance view of which tasks need to be completed for which classes.<br><br>Tasks and login info are stored using Firebase, " +
                 "allowing users to access their task data from anywhere. Tasks stay synced across devices even without refreshing.");
         }
-        else if (this.classList.contains("project2")) {
-            $(".carousel-inner").html(
-                "<div class='item active'><img src='img/project2/1.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project2/2.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project2/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project2/4.jpg' class='d-block img-fluid'></div>"
-            );
-
-            $(".modal-footer").html("<div class='modal-title'>Domino's Address Checker<br><a href='https://gavingt.github.io/address-checker' target='_blank'>gavingt.github.io/address-checker</a></div>" +
-                "I wrote this app to rectify a shortcoming in the computer system of the Domino's store in which I worked. When an employee takes a delivery order over the phone, the system " +
-                "provides no way to verify whether a given address is actually in the store's delivery area. This led to significant losses for the store, as orders were prepared and then " +
-                "cancelled only after it became apparent that the customer was out of the delivery area.<br><br>With this app, the user begins to type an address and is given relevant autocomplete " +
-                "suggestions taken from the Google Places API. Upon making a selection, the address is shown on a map along with the store's delivery boundaries. The app tells the user if the address " +
-                "is within the store's boundaries, and if it's not then it tells the user the phone number of the correct store that the customer should call."
-            );
-        }
-        else if (this.classList.contains("project3")) {
-            $(".carousel-inner").html(
-                "<div class='item active'><img src='img/project3/1.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project3/2.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project3/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project3/4.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project3/5.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project3/6.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project3/7.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project3/8.jpg' class='d-block img-fluid'></div>"
-            );
-
-            $(".modal-footer").html("<div class='modal-title'>Store 7667 Delivery Maps<br><a href='http://goo.gl/nfQDfa' target='_blank'>goo.gl/nfQDfa</a></div>" +
-                "I wrote this Android app to help delivery drivers at the Domino's store in which I worked. It contains all the apartment and hotel maps for our delivery area, allowing drivers " +
-                "to quickly find their destinations. The user can employ voice search or a touch menu to find the relevant map. The app also contains a list of known gate codes in the area, " +
-                "which any user can update on-the-fly. Store-wide use of my app led to a significant increase in on-time deliveries, an important metric for any delivery restaurant."
-
-            );
-        }
         else if (this.classList.contains("project4")) {
             $(".carousel-inner").html(
                 "<div class='item active'><img src='img/project4/1.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project4/2.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project4/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project4/4.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project4/5.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project4/6.jpg' class='d-block img-fluid'>" +
-                "</div><div class='item'><img src='img/project4/7.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project4/8.jpg' class='d-block img-fluid'></div>"
+                "</div><div class='item'><img src='img/project4/3.jpg' class='d-block img-fluid'></div><div class='item'><img src='img/project4/4.jpg' class='d-block img-fluid'></div>"
             );
 
-            $(".modal-footer").html("<div class='modal-title'>Delivery Tip Tracker<br>Pro version: <a href='http://goo.gl/KD2y1t' target='_blank'>goo.gl/KD2y1t</a> &nbsp;| &nbsp;Free version: <a href='https://goo.gl/E8xff3' target='_blank'>goo.gl/E8xff3</a></div>" +
-                "Delivery Tip Tracker is an Android app that helps delivery drivers quickly track their tips. Drivers enter their tips using voice or through a simple touch interface. The app uses GPS to calculate the distance driven on " +
-                "each delivery, yielding useful statistics such as average tip per mile driven and average distance per delivery. The Pro version stores comprehensive tip history with daily, monthly, yearly, and all-time views, and even " +
-                "the ability to see their &quot;high scores&quot; in various stats. Users also have access to cloud-based backup and restore functionality, so they never have to worry about losing their data.<br><br>" +
-                "As of July 2017, the Pro version has accumulated about 500 purchases at $2.99 and the Free version has about 4,000 installs. The Pro version has a user rating of 4.58/5 stars, while the Free version is rated 4.26/5 stars."
+            $(".modal-footer").html("<div class='modal-title'>Store 7667 Address Verifier<br><a href='https://gavingt.github.io/address-checker' target='_blank'>gavingt.github.io/address-checker</a></div>" +
+                "I wrote this app to rectify a shortcoming in the computer system of the Domino's store I worked at. When an employee took a delivery order over the phone, the system " +
+                "provided no way to verify whether a given address was actually in the store's delivery area. This led to significant losses for the store, as orders were prepared and then " +
+                "cancelled only after it became apparent that the customer was out of the delivery area.<br><br>With this app, the user begins to type an address and is given relevant autocomplete " +
+                "suggestions taken from the Google Places API. Upon making a selection, the address is shown on a map along with the store's delivery boundaries. The app tells the user if the address " +
+                "is within the store's boundaries, and if it's not then it tells the user the phone number of the correct store that the customer should call."
             );
         }
 
