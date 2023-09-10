3eea3e085c99b14fb6a93548ea18082639e42baa
Gavin Wright <gavingt@gmail.com>
2023-09-10 12:57:57 -0700

    Add border to selected thumbnail

--- js/sliders.js
+++ js/sliders.js
@@ -1,5 +1,7 @@
 var currentSliderNumber
 
+// TODO: Create array of photos, including their sizes.
+
 // Preload images so they're hopefully ready when the user wants to see them.
 $.preload = function () {
     for (var i = 0; i < arguments.length; i++) {
@@ -15,7 +17,11 @@ $.preload(
     "img/slider-photos/5a.jpeg", "img/slider-photos/5b.jpeg",
     "img/slider-photos/6a.jpeg", "img/slider-photos/6b.jpeg",
     "img/slider-photos/7a.jpeg", "img/slider-photos/7b.jpeg",
-    "img/slider-photos/8a.jpeg", "img/slider-photos/8b.jpeg"
+    "img/slider-photos/8a.jpeg", "img/slider-photos/8b.jpeg",
+    "img/slider-photos/9a.jpeg", "img/slider-photos/9b.jpeg",
+    "img/slider-photos/10a.jpeg", "img/slider-photos/10b.jpeg",
+    "img/slider-photos/11a.jpeg", "img/slider-photos/11b.jpeg",
+    "img/slider-photos/12a.jpeg", "img/slider-photos/12b.jpeg",
 );
 
 
@@ -23,49 +29,66 @@ $.preload(
 changeSliderPhotos(1, 2048, 1152)
 
 
-$(".thumbnail1").on("click", function () {
+$("#thumbnail1").on("click", function () {
     changeSliderPhotos(1, 2048, 1152)
 });
 
-$(".thumbnail2").on("click", function () {
+$("#thumbnail2").on("click", function () {
     changeSliderPhotos(2, 2000, 1500)
 });
 
-$(".thumbnail3").on("click", function () {
+$("#thumbnail3").on("click", function () {
     changeSliderPhotos(3, 1056, 1500)
 });
 
-$(".thumbnail4").on("click", function () {
+$("#thumbnail4").on("click", function () {
     changeSliderPhotos(4, 2048, 1152)
 });
 
-$(".thumbnail5").on("click", function () {
+$("#thumbnail5").on("click", function () {
     changeSliderPhotos(5, 2048, 1365)
 });
 
-$(".thumbnail6").on("click", function () {
+$("#thumbnail6").on("click", function () {
     changeSliderPhotos(6, 2000, 1500)
 });
 
-$(".thumbnail7").on("click", function () {
+$("#thumbnail7").on("click", function () {
     changeSliderPhotos(7, 2048, 1365)
 });
 
-$(".thumbnail8").on("click", function () {
+$("#thumbnail8").on("click", function () {
     changeSliderPhotos(8, 2000, 1500)
 });
 
+$("#thumbnail9").on("click", function () {
+    changeSliderPhotos(9, 2048, 1365)
+});
+
+$("#thumbnail10").on("click", function () {
+    changeSliderPhotos(10, 2001, 1500)
+});
+
+$("#thumbnail11").on("click", function () {
+    changeSliderPhotos(11, 2000, 1500)
+});
+
+$("#thumbnail12").on("click", function () {
+    changeSliderPhotos(12, 1125, 1500)
+});
 
 function changeSliderPhotos(sliderNumber, actualPhotoWidth, actualPhotoHeight) {
     // Prevent the currently selected slider from being selected again.
     if (currentSliderNumber == sliderNumber) return
     currentSliderNumber = sliderNumber
 
+    setBorderForSelectedThumbnail(sliderNumber)
+
     var adjustedWidth = actualPhotoWidth
     var adjustedHeight = actualPhotoHeight
-    // Calculate new slider dimensions based on photo. Prevent the slider height from exceeding 900px.
-    if (actualPhotoHeight > 900) {
-        adjustedHeight = 900
+    // Calculate new slider dimensions based on photo. Prevent the slider height from exceeding 850px.
+    if (actualPhotoHeight > 850) {
+        adjustedHeight = 850
         var scaleFactor = adjustedHeight / actualPhotoHeight
         adjustedWidth = actualPhotoWidth * scaleFactor
     }
@@ -92,4 +115,17 @@ function changeSliderPhotos(sliderNumber, actualPhotoWidth, actualPhotoHeight) {
             startingPosition: "2%",
             makeResponsive: true
         });
+}
+
+
+function setBorderForSelectedThumbnail(sliderNumber) {
+    var index = 1
+    $('#thumbnail_container img').each(function () {
+        if (index == sliderNumber) {
+            $(this).css('border', '6px solid #4798d1')
+        } else {
+            $(this).css('border', '')
+        }
+        index++
+    })
 }
