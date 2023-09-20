bcbc786d197527106ea3084f80112b9ee28d2c74
Gavin Wright <gavingt@gmail.com>
2023-09-20 09:39:07 -0700

    Remove Pexels photo, add Buckinham Palace photo

--- js/sliders.js
+++ js/sliders.js
@@ -21,16 +21,7 @@ var photosToDisplay = [
     ["img/slider-photos/15a.jpg", "img/slider-photos/15b.jpg", 1140, 1800],
     ["img/slider-photos/16a.jpg", "img/slider-photos/16b.jpg", 1772, 1423],
     ["img/slider-photos/17a.jpg", "img/slider-photos/17b.jpg", 2000, 1484],
-    ["img/slider-photos/18a.jpg", "img/slider-photos/18b.jpg", 2048, 1374],
-    ["img/slider-photos/19a.jpg", "img/slider-photos/19b.jpg", 2000, 1500],
-    ["img/slider-photos/20a.jpg", "img/slider-photos/20b.jpg", 2048, 1152],
-    ["img/slider-photos/21a.jpg", "img/slider-photos/21b.jpg", 2048, 1365],
-    ["img/slider-photos/22a.jpg", "img/slider-photos/22b.jpg", 2000, 1500],
-    ["img/slider-photos/23a.jpg", "img/slider-photos/23b.jpg", 2001, 1500],
-    ["img/slider-photos/24a.jpg", "img/slider-photos/24b.jpg", 1700, 1800],
-    ["img/slider-photos/25a.jpg", "img/slider-photos/25b.jpg", 2200, 2000],
-    ["img/slider-photos/26a.jpg", "img/slider-photos/26b.jpg", 1500, 1500],
-    ["img/slider-photos/27a.jpg", "img/slider-photos/27b.jpg", 1333, 2000],
+    ["img/slider-photos/18a.jpg", "img/slider-photos/18b.jpg", 2000, 1500],
 ]
 
 // Preload images so they're hopefully ready when the user wants to see them.
@@ -136,42 +127,6 @@ $("#thumbnail17").on("click", function () {
 
 $("#thumbnail18").on("click", function () {
     changeSliderPhotos(18)
-});
-
-$("#thumbnail19").on("click", function () {
-    changeSliderPhotos(19)
-});
-
-$("#thumbnail20").on("click", function () {
-    changeSliderPhotos(20)
-});
-
-$("#thumbnail21").on("click", function () {
-    changeSliderPhotos(21)
-});
-
-$("#thumbnail22").on("click", function () {
-    changeSliderPhotos(22)
-});
-
-$("#thumbnail23").on("click", function () {
-    changeSliderPhotos(23)
-});
-
-$("#thumbnail24").on("click", function () {
-    changeSliderPhotos(24)
-});
-
-$("#thumbnail25").on("click", function () {
-    changeSliderPhotos(25)
-});
-
-$("#thumbnail26").on("click", function () {
-    changeSliderPhotos(26)
-});
-
-$("#thumbnail27").on("click", function () {
-    changeSliderPhotos(27)
 });
 
 
