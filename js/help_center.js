f48ffeed31593e45f16134c30e440512f17bdfc5
Gavin <gavin@picturekeeper.com>
2024-02-13 20:01:18 -0700

    Various changes

--- js/help_center.js
+++ js/help_center.js
@@ -11,13 +11,84 @@ $("#modal-close-button").click(function () {
     $('#platform-modal').modal("hide")
 })
 
-// When the platform modal is shown, set modal-product-selected-label text and the visible platforms.
+// When the platform modal is shown, set modal-product-selected-label text and the available platforms.
 $('#platform-modal').on('show.bs.modal', function () {
     var modal = $(this)
     modal.find('#modal-product-selected-label').text("You selected " + selectedProductName)
-    // TODO: Set visible platforms
+    setPlatformOpacitiesAndOrder()
 })
 
 $("#modal-change-selected-product").click(function () {
-
+    $('#platform-modal').modal("hide")
 })
+
+
+
+
+function setPlatformOpacitiesAndOrder() {
+    var platformGrid = $('#platform-grid')
+    var ios = $('#platform-ios').parent()
+    var android = $('#platform-android').parent()
+    var windows = $('#platform-windows').parent()
+    var mac = $('#platform-mac').parent()
+    var platforms = [ios, android, windows, mac]
+
+    if (selectedProductName == "Picture Keeper Connect") {
+        ios.css('opacity', '1')
+        android.css('opacity', '1')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    } else if (selectedProductName == "Picture Keeper") {
+        ios.css('opacity', '0.15')
+        android.css('opacity', '0.15')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    } else if (selectedProductName == "Picture Keeper For Android") {
+        ios.css('opacity', '0.15')
+        android.css('opacity', '1')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    } else if (selectedProductName == "Picture Keeper Pro") {
+        ios.css('opacity', '0.15')
+        android.css('opacity', '0.15')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    } else if (selectedProductName == "Picture Keeper Pro SSD") {
+        ios.css('opacity', '0.15')
+        android.css('opacity', '0.15')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    } else if (selectedProductName == "Picture Keeper Pro HDD") {
+        ios.css('opacity', '0.15')
+        android.css('opacity', '0.15')
+        windows.css('opacity', '1')
+        mac.css('opacity', '1')
+    }
+
+    // Detach all platforms.
+    platforms.forEach(element => {
+        element.detach()
+    })
+
+    // Attach only the platforms that are available first.
+    platforms.forEach(element => {
+        if (element.css('opacity') == 1) {
+            element.appendTo(platformGrid)
+            //element.css('pointer-events','')
+        }
+    })
+
+    // Attach the unavailable platforms last.
+    platforms.forEach(element => {
+        if (element.css('opacity') == 0.15) {
+            element.appendTo(platformGrid)
+            //element.css('pointer-events','none')
+        }
+    })
+
+    ios.click(function () {
+        $('.toast').toast('show')
+        //alert("adsf")
+    })
+
+}
